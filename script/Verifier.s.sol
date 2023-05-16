f2a188b36b7210d2d0a1f57b2543f7f82244c544
echo <hujw77@gmail.com>
2023-05-16 14:20:08 +0800

    rename contract

--- script/Verifier.s.sol
+++ script/Verifier.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract VerifierScript is Script {
     function setUp() public {}
 
     function run() public {
