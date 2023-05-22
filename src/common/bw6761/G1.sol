0ae9606107f0cc814ad9a1c6e98c58dd6d7fc3f1
echo <hujw77@gmail.com>
2023-05-22 11:28:27 +0800

    Bw6G1.mul

--- src/common/bw6761/G1.sol
+++ src/common/bw6761/G1.sol
@@ -1,26 +1,49 @@
 pragma solidity ^0.8.17;
 
 import "./Fp.sol";
+import "./Fr.sol";
 
 struct Bw6G1Affine {
     Bw6Fp x;
     Bw6Fp y;
 }
 
-struct Bw6G1Projective {
-    Bw6Fp x;
-    Bw6Fp y;
-    Bw6Fp z;
-}
+library BW6G1Affine {
+    // BW6_G1_MUL
+    uint8 private constant G1_MUL = 0x14;
 
-library BW6G1Affine {}
-
-library BW6G1Projective {
-    function zero() internal pure returns (Bw6G1Projective memory) {
-        return Bw6G1Projective(
-            BW6FP.one(),
-            BW6FP.one(),
+    function zero() internal pure returns (Bw6G1Affine memory) {
+        return Bw6G1Affine(
+            BW6FP.zero(),
             BW6FP.zero()
         );
+    }
+
+    function mul(Bw6G1Affine memory self, Bw6Fr memory scalar) internal pure {
+        uint[8] memory input;
+        input[0] = self.x.a;
+        input[1] = self.x.b;
+        input[2] = self.x.c;
+        input[3] = self.y.a;
+        input[4] = self.y.b;
+        input[5] = self.y.c;
+        input[6] = scalar.a;
+        input[7] = scalar.b;
+        uint[6] memory output;
+
+        assembly ("memory-safe") {
+            if iszero(staticcall(gas(), G1_MUL, input, 256, output, 192)) {
+                let pt := mload(0x40)
+                returndatacopy(pt, 0, returndatasize())
+                revert(pt, returndatasize())
+            }
+        }
+
+        self.x.a = output[0];
+        self.x.b = output[1];
+        self.x.c = output[2];
+        self.y.a = output[3];
+        self.y.b = output[4];
+        self.y.c = output[5];
     }
 }
--- src/common/piop/AffineAddition.sol
+++ src/common/piop/AffineAddition.sol
@@ -19,7 +19,7 @@ library BasicProtocol {
         AffineAdditionEvaluations memory self,
         Bw6Fr memory phi,
         Bw6Fr memory zeta_minus_omega,
-        Bw6Fr[2] memory commitments
+        Bw6G1Affine[2] memory commitments
     ) internal view {
         Bw6Fr memory b = self.bitmask;
         Bw6Fr memory x1 = self.partial_sums[0];
@@ -27,6 +27,13 @@ library BasicProtocol {
         Bw6Fr memory x2 = self.keyset[0];
         Bw6Fr memory y2 = self.keyset[1];
 
-        Bw6G1Projective memory r_comm = BW6G1Projective.zero();
+        Bw6G1Affine memory r_comm = BW6G1Affine.zero();
+        // X3 := acc_x polynomial
+        // Y3 := acc_y polynomial
+        // a1_lin = b(x1-x2)^2.X3 + (1-b)Y3
+        // a2_lin = b(x1-x2)Y3 + b(y1-y2)X3 + (1-b)X3 // *= phi
+        // X3 term = b(x1-x2)^2 + b(y1-y2)phi + (1-b)phi
+        // Y3 term = (1-b) + b(x1-x2)phi
+        // ...and both multiplied by (\zeta - \omega^{n-1}) // = zeta_minus_omega_inv
     }
 }
