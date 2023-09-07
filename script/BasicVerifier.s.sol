de29a999e4e3defbd4d07fdb44da734fda38a2e0
echo <hujw77@gmail.com>
2023-09-07 10:40:28 +0800

    debug script

--- script/BasicVerifier.s.sol
+++ script/BasicVerifier.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
+import "../test/BasicVerifier.t.sol";
 
-contract VerifierScript is Script {
+contract BasicVerifierScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        new BasicVerifierTest();
     }
 }
